628160a562fc48b3d4e4b16c0be7beb03cc3e812
lnalmeida <l.n.almeida.ti@gmail.com>
2022-01-21 07:58:50 -0300

    versão 2 - score funcional, min-max

--- script.js
+++ script.js
@@ -5,46 +5,71 @@ const btnAgain = document.querySelector('.again');
 const number = document.querySelector('.number');
 const guess = document.querySelector('.guess');
 const btnCheck = document.querySelector('.check');
-const score = document.querySelector('.score');
-const highscore = document.querySelector('.highscore');
+const scoreText = document.querySelector('.score');
+const highscoreText = document.querySelector('.highscore');
 
-let randomNumber = Math.floor(Math.random() * 20)+1;
+let randomNumber = Math.trunc(Math.random() * 20)+1;
 console.log(`Número secreto: ${randomNumber}`);
-let scoreNumber = 0;
-let highscoreNumber = window.localStorage.getItem('highscore');
-highscore.innerText = highscoreNumber;
+let score = 20;
+scoreText.innerText = score;
+let highscore = 0
+highscoreText.innerText = highscore;
 
 const checkNumber = (guessValue) => {
-    console.log(guessValue);
-    score.innerText = scoreNumber;
-    if (parseInt(guessValue) === randomNumber) {
-        message.innerText = 'Você acertou!';
-        document.body.classList.toggle('win');
-        scoreNumber+=10;
-        if (scoreNumber > highscoreNumber) {
-            window.localStorage.setItem('highscore', scoreNumber);
-            highscore.innerText = highscoreNumber;
-        } 
-        score.innerText = scoreNumber;
-    } else if (parseInt(guessValue) > randomNumber) {
-        message.innerText = 'Muito alto!';
-    } else {
-        message.innerText = 'Muito baixo!';
-    }
+        if(!guessValue) {
+           message.innerText = 'Digite um número'; 
+        } else if (guessValue === randomNumber) {
+            message.innerText = 'Você Acertou';
+            document.body.classList.add('win');
+            number.innerText = randomNumber;
+            if (score > highscore) {
+                highscore = score;
+                highscoreText.innerText = highscore;
+            } 
+        } else if (guessValue > randomNumber) {
+            if (score > 1) {
+                score--;
+                message.innerText = 'Muito alto';
+                scoreText.innerText = score;
+            } else {
+                endGame();
+            }
+        } else if (guessValue < randomNumber) {
+            if (score > 1) {
+                score--;
+                message.innerText = 'Muito baixo';
+                scoreText.innerText = score;
+            } else {
+                endGame();
+            }
+        }     
 }
 
 
 const resetGame = () => {
-    randomNumber = Math.floor(Math.random() * 20)+1;
-    document.body.classList.toggle('win');
+    if (score === 0) {
+        score = 20
+    }
+    randomNumber = Math.trunc(Math.random() * 20)+1;
+    console.log(`Número secreto: ${randomNumber}`);
+    number.innerText = '?';
+    document.body.classList.remove('win');
     message.innerText = 'Escolha um número entre 0 e 20';
-    highscore.innerText = highscoreNumber;
     guess.value = '';
+    scoreText.innerText = score;
+}
+
+const endGame = () => {
+    message.innerText = 'Você perdeu';
+    scoreText.innerText = '0';
+    if(score == 0) {
+        score = 20;
+    }
 }
 
 
 
 btnAgain.addEventListener('click', resetGame);
-btnCheck.addEventListener('click', () => checkNumber(guess.value));
+btnCheck.addEventListener('click', () => checkNumber(Number(guess.value)));
 
 
